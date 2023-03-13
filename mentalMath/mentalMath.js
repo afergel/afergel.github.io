79d33400a332f4e9b7735871d95a31c2f01623da
afergel <fergel.andrew@gmail.com>
2023-03-13 15:41:48 -0500

    Created basic gameplay loop structure

--- mentalMath/mentalMath.js
+++ mentalMath/mentalMath.js
@@ -1,3 +1,5 @@
+var NUM_OF_QUESTIONS = 10;
+
 window.onload = () =>
 {
     addSliderEvent();
@@ -32,7 +34,19 @@ function addSliderEvent()
 function addStartEvent()
 {
     startBtn = document.getElementById("start");
-    startBtn.addEventListener("click", startGame);
+    startBtn.addEventListener("click", () =>
+    {
+        // Check to see if any "types of questions" boxes are checked before starting
+        if (document.getElementById("add").checked || document.getElementById("subt").checked ||
+            document.getElementById("mult").checked || document.getElementById("divis").checked)
+        {
+            startGame();
+        }
+        else
+        {
+            alert("Please choose at least one question type!");
+        }
+    });
 }
 
 function startGame()
@@ -43,13 +57,54 @@ function startGame()
     var mainMenu = document.getElementById("mainMenu").cloneNode(true);
     gameArea.textContent = '';
 
+    var questionDisplay = document.createElement("h1");
+    questionDisplay.id = "questionDisplay";
+    gameArea.appendChild(questionDisplay);
 
+    var textInput = document.createElement("input");
+    textInput.type = 'text';
+    gameArea.appendChild(textInput);
 
+    var currentQuestion = 1;
+    var data = []; // info about the current question
 
+    function displayQuestion(questionNum)
+    {
+        if (questionNum <= NUM_OF_QUESTIONS)
+        {
+            textInput.value = '';
+            data = generateQuestion(true, true, true, true, 1);
+            questionDisplay.textContent = data[0];
+            textInput.addEventListener("input", checkAnswer);
+        }
+        else
+        {
+            // Reload the main menu upon completion
+            gameArea.textContent = '';
+            gameArea.appendChild(mainMenu);
+            addSliderEvent();
+            addStartEvent();
+        }
+    }
 
-    // Re-load the main menu
-    alert("Testing");
-    gameArea.appendChild(mainMenu);
-    addSliderEvent();
-    addStartEvent();
+    function checkAnswer()
+    {
+        if (textInput.value == data[1])
+        {
+            textInput.removeEventListener("input", checkAnswer);
+            displayQuestion(++currentQuestion);
+        }
+    }
+
+    displayQuestion(currentQuestion);
+}
+
+/*
+ * Generates a math question. Returns an array with two elements:
+ *      - A string representation of the question
+ *      - The answer as an integer
+ */
+function generateQuestion(add, subt, mult, divis, difficulty)
+{
+    return ["2 + 2", 4];
 }
